e67988e974bc8bf2e5f9fc3a1b037c2583b95a54
rowooncoding <dev.rowoon@gmail.com>
2022-12-29 13:14:43 +0900

    조건문 수정

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -3,7 +3,7 @@
 /* Condition                                                              */
 /* ---------------------------------------------------------------------- */
 
-let number = prompt('숫자를 입력해 주세요', 0);
+// let number = prompt('숫자를 입력해 주세요', 0);
 
 // if(number > 0) {
 //   console.log(1);
@@ -13,12 +13,12 @@ let number = prompt('숫자를 입력해 주세요', 0);
 //   console.log(0);
 // }
 
-let message = 
-(number > 0) ? '1' : 
-(number < 0) ? '-1' : 
-'아무것도 아닙니다';
+// let message = 
+// (number > 0) ? '1' : 
+// (number < 0) ? '-1' : 
+// '아무것도 아닙니다';
 
-console.log(message);
+// console.log(message);
 
 
 // 그 영화 봤니?
@@ -29,14 +29,25 @@ console.log(message);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = prompt('영화 봄?');
 
 // 영화 볼거니?
-let goingToWatchMovie;
+let goingToWatchMovie = prompt('영화 안봄?');
 
 
 // if 문(statement)
+if(didWatchMovie === 'yes') {
+  console.log('합격');
+}else if(goingToWatchMovie === 'yes'){
+  console.log(('합격'));
+}else {
+  console.log('불합격ㅛㄷㄴ');
+}
 
+
+let movieMessage =
+(didWatchMovie.includes('yes')) ? '굿' :
+(goingToWatchMovie === 'yes') ? '굿' : '배드';
 // else 절(caluse)
 
 // else if 복수 조건 처리
