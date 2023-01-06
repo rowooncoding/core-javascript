64e2b09daf9b64512cd4d959446c9ee9a9a626f4
rowooncoding <dev.rowoon@gmail.com>
2023-01-06 15:39:36 +0900

    유틸함수 모음 파일 생성

---
+++ client/chapter/dom/01.traversal.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* ---------------------------------------------------------------------- */
+/* DOM traversal                                                          */
+/* ---------------------------------------------------------------------- */
+
+
+/* 모든 노드에서 사용 */
+// - parentNode
+// - childNodes
+// - firstChild
+// - lastChild
+// - previousSibling
+// - nextSibling
+
+/* 요소 노드에서만 사용 가능 */
+// - parentElement
+// - children
+// - firstElementChild
+// - lastElementChild
+// - previousElementSibling
+// - nextElementSibling
+
+/* 문서 대상 찾기 */
+// - getElementById
+// - getElementsByTagName
+// - getElementsByClassName
+// - querySelector
+// - querySelectorAll
+// - closest
+
+// let first = document.querySelector('.first');
+let span = document.querySelectorAll('span');
+
+let first = span[0];
+let second = span[1];
+
+
+
+/* 문서 대상 확인 */
+// - matches
+// - contains
+
+
---
+++ client/lib/dom/getNode.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+function getNode(node){
+
+  if(typeof node !== 'string'){
+    throw new Error('getNode 함수의 인자는 문자타입 이어야 합니다.')
+  }
+
+  return document.querySelector(node)
+}
+
+function getNodes(node){
+
+  if(typeof node !== 'string'){
+    throw new Error('getNode 함수의 인자는 문자타입 이어야 합니다.')
+  }
+
+  return document.querySelectorAll(node)
+}
