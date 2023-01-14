cb82351a0bc4be60f29b85c96a4836987174cbce
rowooncoding <dev.rowoon@gmail.com>
2023-01-14 12:48:04 +0900

    코드 설명 추가

--- client/main.js
+++ client/main.js
@@ -29,33 +29,39 @@ import {
 
 
 
-
+//1. 입력할 첫번째 칸과 두번째 칸의 노드를 가져옴
 const firstInput = getNode('#firstNumber');
 const secondInput = getNode('#secondNumber');
+//2. 제출버튼의 노드를 가져옴
 const done = getNode('#done');
+//3. 결과를 출력하는 부분의 노드를 가져옴
 const result = getNode('.result');
 
 
 
-
+//5. 클릭이벤트 핸들러 받는 함수
 function handler(e){
+  // 6. 새로고침 안되도록
   e.preventDefault();
-
+  // 7. 첫번째 칸과 두번째 칸의 벨류를 가져오는 함수 적용
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
+  // 8. sum 함수를 사용해서 더해주기
   let total = sum(firstValue, secondValue)
 
 
-  
+  // 9. result 쪽에 클리어 컨텐츠를 위한 것
   clearContents(result);
-
+  //10. 넣어줌
   insertLast(result,total);
-  
+  // result에 값이 들어갈때 clearContent로 인해서 빨간 줄이 없어지고 값이 들어간다
+  // 자바스크릡트는 한 줄 씩 실행되기 때문
 }
 
-
-
+// 추가 : 값을 화살표로 올리거나 내릴때 자동으로 값이 변환하도록 하는 이벤트 구현
+// +2 change이벤트 함수
 function inputHandler(){
+  // +3 첫번째와 두번째 칸에 벨류를 받아오고 합을 도출(위와 같다)
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
   let total = sum(firstValue, secondValue)
@@ -68,8 +74,8 @@ function inputHandler(){
 }
 
 
-
+// 4. 출력칸에 클릭 이벤트
 done.addEventListener('click',handler)
-
+// +1 change 라는 이벤트를 삽입
 firstInput.addEventListener('change',inputHandler)
 secondInput.addEventListener('change',inputHandler)
