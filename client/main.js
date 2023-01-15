7b1894a5d5c38b112b092babfb882671a7f8e07a
rowooncoding <dev.rowoon@gmail.com>
2023-01-15 14:54:14 +0900

    사용 할 요소들 변수에 할당하기

--- client/main.js
+++ client/main.js
@@ -9,7 +9,9 @@ const resultArea = getNode('.result');
 
 function handler(e){
   e.preventDefault();
-  
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length-1)];
 }
 
 
