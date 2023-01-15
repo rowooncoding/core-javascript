dc1b32235c11e70ade2fbef09f14746adb110657
rowooncoding <dev.rowoon@gmail.com>
2023-01-15 15:02:33 +0900

    validaition 제작

--- client/main.js
+++ client/main.js
@@ -13,6 +13,15 @@ function handler(e){
   let list = jujeobData(name);
   let pick = list[getRandom(list.length-1)];
 
+  if(!name){
+    showAlert('.alert-error','잘못된 이름입니다!',2000);
+    return
+  }
+  if(isNumericString){
+    showAlert('alert.error','이름을 정확히 입력해 주세요!',2000)
+    return
+  }
+
   clearContents(resultArea);
   insertLast(resultArea,pick);
 }
