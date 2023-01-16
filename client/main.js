dabaa6e981cd622ffa5b2d83fde386f0bbce54e8
rowooncoding <dev.rowoon@gmail.com>
2023-01-16 11:13:32 +0900

    클로저 추가

--- client/main.js
+++ client/main.js
@@ -6,22 +6,23 @@ let rollingDicebutton = getNode('.buttonGroup > button:nth-child(1)');
 const handlerRollingDice = () => {
   let stopAnimation
   let isRolling = false;
-  
-  return function(){if(!isRolling){
-    stopAnimation =  setInterval(diceAnimation,100)
+
+  return function(){
+  if(!isRolling){
+  stopAnimation =  setInterval(diceAnimation,100)
   }else {
-    clearInterval(stopAnimation);
+  clearInterval(stopAnimation);
   }
   // 계속 false 값이 나오니까 값 반전해서 else값이 가능하도록
   isRolling = !isRolling;
 }
   
 }
+// 현재는 함수 자체가 튀어 나와서 실행 안됨!
 
 
-
-rollingDicebutton.addEventListener('click',handlerRollingDice);
-
+rollingDicebutton.addEventListener('click',handlerRollingDice());
+// 안에있는 함수를 실행하기 위해서 클릭할때 실행 -> 한번 더 실행
 
 
 
