97de1a0a11beb021b28613b68c166fbd352931db
rowooncoding <dev.rowoon@gmail.com>
2023-01-16 10:41:50 +0900

    이벤트 핸들러 연결

--- client/main.js
+++ client/main.js
@@ -1,1 +1,19 @@
-import { diceAnimation } from "./lib/index.js";
+import { diceAnimation, getNode } from "./lib/index.js";
+
+let rollingDicebutton = getNode('.buttonGroup > button:nth-child(1)');
+
+const handlerRollingDice = () => {
+
+}
+
+
+
+rollingDicebutton.addEventListener('click',handlerRollingDice);
+
+
+
+
+
+
+
+
