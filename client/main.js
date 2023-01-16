6901a7873b344a4f67163cdd411d348b37910d89
rowooncoding <dev.rowoon@gmail.com>
2023-01-16 10:47:48 +0900

    setTimeout과 clearTimeout  개념

--- client/main.js
+++ client/main.js
@@ -2,6 +2,12 @@ import { diceAnimation, getNode } from "./lib/index.js";
 
 let rollingDicebutton = getNode('.buttonGroup > button:nth-child(1)');
 
+let id = setInterval(() => {
+  
+}, 1000);
+
+clearInterval(id)
+
 const handlerRollingDice = () => {
   diceAnimation();
 }
