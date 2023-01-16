5a5b58c881bad070d402fcb7d76c73cd85a2dd77
rowooncoding <dev.rowoon@gmail.com>
2023-01-16 10:44:17 +0900

    클릭 할때마다 돌아가게 지정해놓은 함수 적용

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import { diceAnimation, getNode } from "./lib/index.js";
 let rollingDicebutton = getNode('.buttonGroup > button:nth-child(1)');
 
 const handlerRollingDice = () => {
-
+  diceAnimation();
 }
 
 
