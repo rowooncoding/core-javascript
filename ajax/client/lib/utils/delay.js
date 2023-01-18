3073133443d08bb1e867d1a1b1936ccec90d8ad8
rowooncoding <dev.rowoon@gmail.com>
2023-01-18 10:30:56 +0900

    에러 컨트롤

--- ajax/client/lib/utils/delay.js
+++ ajax/client/lib/utils/delay.js
@@ -1,10 +1,11 @@
+
 import { getNode } from '../dom/getNode.js'
 import { isNumber,isObject } from './typeOf.js'
 
 
 
-
 const first = getNode('.first');
+const second = getNode('.second');
 
 
 
@@ -21,49 +22,60 @@ function delay(callback,timeout = 1000){
 delay(()=>{
   first.style.top = '-100px';
   delay(()=>{
+    second.style.left = '100px';
     delay(()=>{
       first.style.top = '0px';
+      second.style.left = '0px';
     })
     first.style.transform = 'rotate(360deg)';
   })
 })
  */
 
+/* 
+delayP()
+.then(()=>{
+  first.style.top = '-100px';
+  return delayP()
+})
+.then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  second.style.left = '100px';
+  return delayP()
+})
+.then(()=>{
+  first.style.top = '0px';
+  second.style.left = '0px';
+})
+ */
 
-// delayP()
-// .then(()=>{
-//   first.style.top = '-100px';
-//   return delayP()
-// })
-// .then(()=>{
-//   first.style.transform = 'rotate(360deg)';
-//   return delayP()
-// })
-// .then(()=>{
-//   first.style.top = '0px';
-// })
-
-function delayP(shouldReject = false, timeout = 1000){
+function delayP(shouldReject = false, timeout = 1000,data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
 
   return new Promise((resolve, reject) => {
     
     setTimeout(() => {
-      !shouldReject ? resolve('성공!') : reject('실패!')
-      // if(!shouldReject){
-      //   resolve('성공!');
-      // }else{
-      //   reject('실패!');
-      // }
+      !shouldReject ? resolve(data) : reject(errorMessage);
     }, timeout);
   })
 }
 
-delay(true);
+
+delayP(false,1000,'진짜 성공','오류가 발생했다!!').then((res)=>{
+  console.log(res); // 진짜 성공
+})
 
 
 // delayP()
-// .then(res => console.log(res))
-// .catch(err => console.log(err))
+// .then(res=>console.log(res))
+// .catch(err=>console.log(err))
+
+
+
+
+
+
+
+// async await
 
 
 
