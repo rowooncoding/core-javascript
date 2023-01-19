f3dfcc61f612a2a0c091a2e6515446e7e2c5e84f
rowooncoding <dev.rowoon@gmail.com>
2023-01-19 15:26:07 +0900

    스토리지 속성 학습

--- client/lib/utils/storage.js
+++ client/lib/utils/storage.js
@@ -8,4 +8,25 @@ const {
   JSON:{stringify:serialize, parse: deserialize}
 }=globalThis
 
-console.log();
+
+
+
+
+
+
+// saveStoreage('name','tiger')
+// loadStoreage('name')
+// deleteStoreage('name')
+
+
+
+
+
+storage.setItem('name','tiger');
+console.log(storage.getItem('name'));
+
+// 제거하는거
+// storage.removeItem('name');
+
+// 한방에 날아가는거
+storage.clear()
