988bd506977ec0cf05a7499670600be6eaae854b
rowooncoding <dev.rowoon@gmail.com>
2023-01-19 15:52:47 +0900

    스토리지 적용하기

--- client/main.js
+++ client/main.js
@@ -1,1 +1,25 @@
-import {} from "./lib/index.js"
+
+
+
+import { text } from 'stream/consumers';
+import {getNode, loadStorage, saveStorage} from "./lib/index.js"
+
+
+
+const textField = getNode('#textField');
+const deleteButton = getNode('input[value="삭제"]');
+
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+
+function inputHandler(){
+  saveStorage('area',textField.value);
+}
+
+
+
+
+textField.addEventListener('input',inputHandler);
