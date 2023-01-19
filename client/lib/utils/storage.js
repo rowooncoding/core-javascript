5827efa2a7322cf94c2e59ef1b3509bafaedcc74
rowooncoding <dev.rowoon@gmail.com>
2023-01-19 15:42:45 +0900

    storeage 함수 만들기

--- client/lib/utils/storage.js
+++ client/lib/utils/storage.js
@@ -38,25 +38,40 @@ const albums = [
   },
 ];
 
-
 // JSON.stringify()
 // serialize()
 // deserialize()
 
 
-function saveStorage(key,value){
-    return new Promise((resolve, reject) => {
-        if(isString(key)){
-        storage.setItem(key,value);
+export function saveStorage(key,value){
+   return new Promise((resolve, reject) => {
+       if(isString(key)){
+        storage.setItem(key,serialize(value));
         resolve();
-        }else{
+       }else{
         reject({message:'key는 문자 타입 이어야 합니다.'});
-        }
-    })
+       }
+   })
+}
+
+export function loadStorage(key){
+  return new Promise((resolve, reject) => {
+    if(isString(key)){
+      resolve(deserialize(storage.getItem(key)))
+    }else{
+      reject({message:'key는 문자 타입 이어야 합니다.'});
+    }
+  })
+}
+
+export function deleteStorage(key){
+  return new Promise((resolve, reject) => {
+    !key ? storage.clear() : storage.removeItem(key);
+    resolve();
+  })
 }
 
 
-saveStorage('name','tiger');
 
 // saveStorage('name','tiger')
 
